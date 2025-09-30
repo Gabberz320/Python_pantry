bb2c3f6b76aafc29e206586f2eecd693bcf2fd2a
Gabby <gvadai91@icloud.com>
2025-09-30 09:57:13 -0500

    Removed get recipe detail query and set get recipe info to true to cut API pulls

--- recipe_requests.py
+++ recipe_requests.py
@@ -2,7 +2,7 @@ import requests
 
 
 
-API_KEY = "ab1ce54d449b4fac9a3d4feb66bb5707"
+API_KEY = "aa16c40bfb484d6dba1f16ac81f769ea"
 API_URL = "https://api.spoonacular.com"
 SEARCH_URL = f"{API_URL}/recipes/complexSearch"
 RECIPE_INFO_URL = "https://api.spoonacular.com/recipes/{id}/information"
@@ -14,7 +14,7 @@ def search_recipes(query, ingredients, cuisine, diet):
     params = {'apiKey': API_KEY, 'number': NUM_RESULTS,
             'query': query, 'includeIngredients': ', '.join(ingredients), 
             'cuisine': ','.join(cuisine),
-            'diet': diet}
+            'diet': diet, 'addRecipeInformation': True}
 
     try: 
         response = requests.get(SEARCH_URL, params=params)
@@ -23,16 +23,7 @@ def search_recipes(query, ingredients, cuisine, diet):
     except requests.exceptions.RequestException as e:
         print(f'An error occured: {e}')
         return None
-# Get the reipe infomation to display from the recipe ID
-def get_recipe_details(recipe_id):
-    try:
-        params = {'apiKey': API_KEY}
-        response = requests.get(RECIPE_INFO_URL.format(id = recipe_id), params = params)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f'Error fetching details: {recipe_id} {e}')
-        return None
+
 
 def main():
     query = []
@@ -43,26 +34,25 @@ def main():
     diet = input('Enter dietary preferences: ').strip()
 
     recipies_found = search_recipes(query,ingredients,cuisine, diet)
+    
 
     if not recipies_found:
         print('Sorry none found')
         return
-    
-    # Search for the recipe information (cooktime, URL, anf title of the recipe)
-    for recipe_summary in recipies_found:
-        recipe_id = recipe_summary['id']
+    else:
 
-        details = get_recipe_details(recipe_id)
+        #Display recipe information
+        for recipe_summary in recipies_found:
+            
+            title = recipe_summary.get('title','N/A')
+            cook_time = recipe_summary.get('readyInMinutes','N/A')
+            source_url = recipe_summary.get('sourceUrl')
 
-        title = details.get('title','N/A')
-        cook_time = details.get('readyInMinutes','N/A')
-        source_url = details.get('sourceUrl')
-
-        if details:
-            print(f'{title}')
-            print(f'Time to cook: {cook_time} minutes')
-            print(f'URL: {source_url}', end = '\n')
-            print('\n')
+            if recipe_summary:
+                print(f'{title}')
+                print(f'Time to cook: {cook_time} minutes')
+                print(f'URL: {source_url}', end = '\n')
+                print('\n')
             
 
 main()
