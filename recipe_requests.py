51a1e755a8695ebc496f3b0749455fd0329db7b9
Gabby <gvadai91@icloud.com>
2025-10-02 10:20:39 -0500

    Updated to sort by missing ingredients and random numbers for offset and number of recipes generated

--- recipe_requests.py
+++ recipe_requests.py
@@ -1,11 +1,12 @@
 import requests
+import random
 
 
-
-API_KEY = "aa16c40bfb484d6dba1f16ac81f769ea"
+API_KEY = "" # add your API key here
 API_URL = "https://api.spoonacular.com"
 SEARCH_URL = f"{API_URL}/recipes/complexSearch"
-NUM_RESULTS = 10
+NUM_RESULTS = random.randint(50, 100)
+NUM_SKIP = random.randint(1, 500)
 
 # search for the recipe with the given ingredients
 # return the list of recipes for future info search
@@ -13,7 +14,7 @@ def search_recipes(ingredients, cuisine, diet):
     params = {'apiKey': API_KEY, 'number': NUM_RESULTS,
             'includeIngredients': ','.join(ingredients), 
             'cuisine': ','.join(cuisine),
-            'diet': diet, 'addRecipeInformation': True, 'ignorePantry': False, 'sort': 'random'}
+            'diet': diet, 'addRecipeInformation': True, 'ignorePantry': False, 'sort': 'min-missing-ingredients', 'offset': NUM_SKIP}
 
     try: 
         response = requests.get(SEARCH_URL, params=params)
@@ -40,14 +41,15 @@ def main():
         return
     else:
 
-        #Display recipe information
-        for recipe_summary in recipies_found:
+       #Display recipe information
+        for recipe_summary in recipies_found[:10]:
             
             title = recipe_summary.get('title','N/A')
             cook_time = recipe_summary.get('readyInMinutes','N/A')
             source_url = recipe_summary.get('sourceUrl', 'N/A')
 
             if recipe_summary:
+
                 print(f'{title}')
                 print(f'Time to cook: {cook_time} minutes')
                 print(f'URL: {source_url}', end = '\n')
