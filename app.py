c57ac41b7f20dfdbd44fd5d9712aaeb208ec2995
Gabby <gvadai91@icloud.com>
2025-10-16 10:59:04 -0500

    Testing some search features in recipe request and added nutrition information to app.py for calorie count

--- app.py
+++ app.py
@@ -203,6 +203,7 @@ def search_recipes():
         "diet": diet,
         "intolerances": allergies,
         "addRecipeInformation": True,
+        "addRecipeNutrition": True,
         "ignorePantry": False,
         "ranking": 2,
         "sort": "min-missing-ingredients",
