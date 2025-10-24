abae1337e5a395a56e757a988569e6801fbdc8d7
gfj2e <gfj2e@outlook.com>
2025-10-24 18:49:50 -0500

    Changed saved_recipe table
    Changed saved_recipe table to match edamam api

--- seed.py
+++ seed.py
@@ -1,23 +1,10 @@
-from sqlalchemy import insert
 from database.connection import db
-from database.models import Ingredient
 from app import app
 
 def main():
     with app.app_context():
         db.drop_all()
         db.create_all()
-        try:
-            with open("database/ingredients.txt", "r") as file:
-                print("Inserting ingredient data...")
-                for line in file:
-                    ingredient = line.strip()
-                    new_ingredient = Ingredient(ingredient_name=ingredient)
-                    db.session.add(new_ingredient)
-                db.session.commit()
-        except FileNotFoundError:
-            print("Error: file ingredients.txt not found")
-
 if __name__ == "__main__":        
     main()
     
