64d7c0c84dcc06a2d53f1818e5eb773c57b0c466
Github-user-2468 <karigan.stewart05@gmail.com>
2026-02-14 13:16:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ bcrypt.init_app(app)
 # ---------------- HOME PAGE ----------------
 @app.route("/")
 def index():
-    return render_template("under_construction.html")
+    return render_template("index.html")
 
 # ---------------- GOOGLE LOGIN ----------------
 @app.route("/google_login")
